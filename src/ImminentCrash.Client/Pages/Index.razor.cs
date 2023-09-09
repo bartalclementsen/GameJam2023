c45394fe3b5a6bb1142aaccd90ebb91152d5234b
Bartal Clementsen <bac@formula.fo>
2023-09-09 10:51:01 +0100

    Add version and first index page

---
+++ src/ImminentCrash.Client/Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace ImminentCrash.Client.Pages
+{
+    public partial class Index
+    {
+        private string _version = "Unknown";
+
+        protected override void OnInitialized()
+        {
+            _version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown";
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
